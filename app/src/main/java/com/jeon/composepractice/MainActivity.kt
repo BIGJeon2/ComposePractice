62b0803db4d9e166a61d466a931e55b5325070ba
BIGJeon2 <robert1157311@gmail.com>
2024-05-14 19:44:43 +0900

    Column Practice

--- app/src/main/java/com/jeon/composepractice/MainActivity.kt
+++ app/src/main/java/com/jeon/composepractice/MainActivity.kt
@@ -3,8 +3,12 @@ package com.jeon.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,7 +19,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jeon.composepractice.ui.theme.ComposePracticeTheme
 
@@ -31,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SimpleCounterBtn()
+                    ColumnPractice()
                 }
             }
         }
@@ -39,20 +45,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SimpleCounterBtn(){
-
-    var count by remember { mutableStateOf(0) }
-
-    Button(
-        onClick = { count++ },
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Text(
-            text = "Count = $count",
-            fontSize = 60.sp
-        )
-    }
-
+fun ColumnPractice(){
+   Column(
+       modifier = Modifier.padding(top = 30.dp, bottom = 30.dp, start = 20.dp, end = 20.dp)
+   ) {
+       Text(
+           text = "This is Column Practice",
+           fontSize = 30.sp
+       )
+       Spacer(modifier = Modifier.padding(30.dp))
+       Divider(
+           thickness = 10.dp,
+           color = Color.Blue
+       )
+       Spacer(modifier = Modifier.padding(30.dp))
+       Text(
+           text = "I Want this view in column1",
+           fontSize = 30.sp,
+       )
+       Text(
+           text = "I Want this view in column2",
+           fontSize = 30.sp,
+       )
+   }
 }
 
 @Preview(showBackground = true)
@@ -63,7 +78,7 @@ fun GreetingPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            SimpleCounterBtn()
+            ColumnPractice()
         }
     }
 }
