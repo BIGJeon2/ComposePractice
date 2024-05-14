92df1c933d927f9359d01518223c23c77f715abb
BIGJeon2 <robert1157311@gmail.com>
2024-05-14 18:38:27 +0900

    Text, Button Practice

--- app/src/main/java/com/jeon/composepractice/MainActivity.kt
+++ app/src/main/java/com/jeon/composepractice/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.jeon.composepractice
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jeon.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +40,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        MyTextEx()
+                        MyBtn()
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyTextEx(){
+    Text(
+        text = "This is Compose",
+        fontSize = 18.sp,
+        color = Color.Red,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(30.dp),
+        style = TextStyle(background = Color.Blue)
+    )
+}
+
+@Composable
+fun MyBtn(){
+
+    val context: Context = LocalContext.current
+
+    Button(onClick = {
+        Log.d("Main", "onClicked")
+        Toast.makeText(context, "버튼이 클릭 되었습니다.", Toast.LENGTH_SHORT).show()
+    },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Yellow,
+            contentColor = Color.Black
+        ),
+        modifier = Modifier
+            .width(120.dp)
+            .height(60.dp),
+    ) {
+        Text(
+            text = "This is Button",
+            lineHeight = 30.sp,
+            fontSize = 20.sp,
+            color = Color.Blue
+        )
     }
 }
 
@@ -45,7 +105,10 @@ fun GreetingPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Greeting("Android")
+            Column {
+                MyTextEx()
+                MyBtn()
+            }
         }
     }
 }
