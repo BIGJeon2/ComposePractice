784d57134ef841df258c4cda06edcfbb71824eaf
BIGJeon2 <robert1157311@gmail.com>
2024-05-16 16:10:45 +0900

    Row+Column Practice

--- app/src/main/java/com/jeon/composepractice/MainActivity.kt
+++ app/src/main/java/com/jeon/composepractice/MainActivity.kt
@@ -3,8 +3,10 @@ package com.jeon.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,57 +33,137 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.jeon.composepractice.ui.theme.ComposePracticeTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                rowPractice()
+                ColumnRowPractice2()
             }
         }
     }
 }
 
 @Composable
-private fun rowPractice() {
-    Row (
+private fun ColumnRowPractice() {
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Gray),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+            .padding(20.dp)
+            .background(Color.Gray)
     ) {
         Text(
-            text = "Item1",
-            style = TextStyle(
-                background = Color.Blue
+            text = "Hi i am Android Native Developer",
+            color = Color.Blue,
+            fontSize = 20.sp
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(Color.Blue),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Left"
+            )
+            Text(
+                text = "Center"
+            )
+            Text(
+                text = "Right"
+            )
+        }
+        Text(
+            text = "BigJeon",
+            color = Color.Red,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+private fun ColumnRowPractice2() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .background(Color.Cyan)
+            .border(
+                border = BorderStroke(5.dp, color = Color.Blue)
             ),
-            fontSize = 30.sp
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(top = 20.dp)
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.test_image),
+                contentDescription = "This is my photo",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(50.dp))
+            )
+        }
+        Text(
+            text = "Big Jeon",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 30.dp)
         )
         Text(
-            text = "Item1",
-            style = TextStyle(
-                background = Color.Green
-            ),
-            fontSize = 30.sp
+            text = "AOS - Developer",
+            fontSize = 15.sp,
+            modifier = Modifier.padding(top = 10.dp)
         )
-        Text(
-            text = "Item1",
-            style = TextStyle(
-                background = Color.Red
-            ),
-            fontSize = 30.sp
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "EMail",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                text = "robert6728@naver.com",
+                fontSize = 15.sp,
+                color = Color.Blue,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Mobile",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                text = "010-4037-6728",
+                fontSize = 15.sp,
+                color = Color.Blue,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
     }
 }
 
@@ -88,6 +171,6 @@ private fun rowPractice() {
 @Composable
 fun GreetingPreview() {
     ComposePracticeTheme {
-        rowPractice()
+        ColumnRowPractice2()
     }
 }
